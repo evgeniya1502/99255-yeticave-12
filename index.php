b50abf83e38d060f7cfe5628ab66eaf5c5bf2bb9
evgeniya1502 <evgeniya1502@yandex.ru>
2022-09-15 11:46:33 +0300

    Удалила лишний код

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ mysqli_set_charset($con, "utf8mb4");
 $sql = "SELECT category, symbol_code FROM categories";
 $result = mysqli_query($con, $sql);
 $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
-//$category_new = array_column($categories, 'category');
+
 
 $sql_slots = "SELECT name, price_start, image, date_fin, IFNULL(MAX(rates.price), lots.price_start) AS price, categories.category FROM lots
 LEFT JOIN rates ON lots.id = rates.lot_id
@@ -22,60 +22,9 @@ GROUP BY lots.id
 ORDER BY lots.date_create DESC";
 $result_slots = mysqli_query($con, $sql_slots);
 $slots = mysqli_fetch_all($result_slots, MYSQLI_ASSOC);
-//var_dump($categories);
 
-
-/*
-$slots = [
-    [
-        'title' => '2014 Rossignol District Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => '10999',
-        'url' => 'img/lot-1.jpg',
-        'fin_date' => '2022-07-08',
-    ],
-
-    [
-        'title' => 'DC Ply Mens 2016/2017 Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => '159999',
-        'url' => 'img/lot-2.jpg',
-        'fin_date' => '2022-07-10',
-    ],
-
-    [
-        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'category' => 'Крепления',
-        'price' => '8000',
-        'url' => 'img/lot-3.jpg',
-        'fin_date' => '2022-07-20',
-    ],
-    [
-        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'category' => 'Ботинки',
-        'price' => '10999',
-        'url' => 'img/lot-4.jpg',
-        'fin_date' => '2022-09-12'
-    ],
-    [
-        'title' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'category' => 'Одежда',
-        'price' => '7500',
-        'url' => 'img/lot-5.jpg',
-        'fin_date' => '2022-07-15',
-    ],
-    [
-        'title' => 'Маска Oakley Canopy',
-        'category' => 'Разное',
-        'price' => '5400',
-        'url' => 'img/lot-6.jpg',
-        'fin_date' => '2022-07-09',
-    ],
-];
-*/
 
 $data = [
-    //'category_new' => $category_new,
     'slots' => $slots,
     'categories' => $categories,
 ];
@@ -86,7 +35,6 @@ $layout_content = include_template('layout.php', [
     'user_name' => $user_name,
     'title' => 'Главная',
     'main_content' => $main_content,
-    //'category_new' => $category_new,
     'categories' => $categories,
 ]);
 echo $layout_content;
